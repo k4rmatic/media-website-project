24cc1dd24ba5ec3582cfab2ab29ab77549defa3e
k4rmatic <aqiba123@gmail.com>
2016-10-28 23:48:42 +0100

    added the flash

--- js/new_tv_shows.js
+++ js/new_tv_shows.js
@@ -1004,4 +1004,109 @@
 				return;
 	}
 
+//The flash
+	//the flash Season 1
+	function TheFlash_s1() {
+				var player = videojs('video');
+			    player.playlist([
+			   		{
+			      	name: 'Season 1 - Episode 1',
+			      	sources: [{ src: 'http://81.111.188.191/media/new_tv_shows/TheFlash/Season01/S01E01.mp4', type: 'video/mp4' }]
+					},
+			    	{
+			      	name: 'Episode 2',
+			      	sources: [{ src: 'http://81.111.188.191/media/new_tv_shows/TheFlash/Season01/S01E02.mp4', type: 'video/mp4' }]
+			    	},
+			    	{
+			      	name: 'Episode 3',
+			      	sources: [{ src: 'http://81.111.188.191/media/new_tv_shows/TheFlash/Season01/S01E03.mp4', type: 'video/mp4' }]
+			    	},
+			    	{
+			      	name: 'Episode 4',
+			      	sources: [{ src: 'http://81.111.188.191/media/new_tv_shows/TheFlash/Season01/S01E04.mp4', type: 'video/mp4' }]
+			    	},
+			    	{
+			      	name: 'Episode 5',
+			      	sources: [{ src: 'http://81.111.188.191/media/new_tv_shows/TheFlash/Season01/S01E05.mp4', type: 'video/mp4' }]
+			    	},
+			    	{
+			      	name: 'Episode 6',
+			      	sources: [{ src: 'http://81.111.188.191/media/new_tv_shows/TheFlash/Season01/S01E06.mp4', type: 'video/mp4' }]
+			    	},
+			    	{
+			      	name: 'Episode 7',
+			      	sources: [{ src: 'http://81.111.188.191/media/new_tv_shows/TheFlash/Season01/S01E07.mp4', type: 'video/mp4' }]
+			    	},
+			    	{
+			      	name: 'Episode 8',
+			      	sources: [{ src: 'http://81.111.188.191/media/new_tv_shows/TheFlash/Season01/S01E08.mp4', type: 'video/mp4' }]
+			    	},
+			    	{
+			      	name: 'Episode 9',
+			      	sources: [{ src: 'http://81.111.188.191/media/new_tv_shows/TheFlash/Season01/S01E09.mp4', type: 'video/mp4' }]
+			    	},
+			    	{
+			      	name: 'Episode 10',
+			      	sources: [{ src: 'http://81.111.188.191/media/new_tv_shows/TheFlash/Season01/S01E10.mp4', type: 'video/mp4' }]
+			    	},
+			    	{
+			      	name: 'Episode 11',
+			      	sources: [{ src: 'http://81.111.188.191/media/new_tv_shows/TheFlash/Season01/S01E11.mp4', type: 'video/mp4' }]
+			    	},
+			    	{
+			      	name: 'Episode 12',
+			      	sources: [{ src: 'http://81.111.188.191/media/new_tv_shows/TheFlash/Season01/S01E12.mp4', type: 'video/mp4' }]
+			    	},
+			    	{
+			      	name: 'Episode 13',
+			      	sources: [{ src: 'http://81.111.188.191/media/new_tv_shows/TheFlash/Season01/S01E13.mp4', type: 'video/mp4' }]
+			    	},
+			    	{
+			      	name: 'Episode 14',
+			      	sources: [{ src: 'http://81.111.188.191/media/new_tv_shows/TheFlash/Season01/S01E14.mp4', type: 'video/mp4' }]
+			    	},
+			    	{
+			      	name: 'Episode 15',
+			      	sources: [{ src: 'http://81.111.188.191/media/new_tv_shows/TheFlash/Season01/S01E15.mp4', type: 'video/mp4' }]
+			    	},
+			    	{
+			      	name: 'Episode 16',
+			      	sources: [{ src: 'http://81.111.188.191/media/new_tv_shows/TheFlash/Season01/S01E16.mp4', type: 'video/mp4' }]
+			    	},
+			    	{
+			      	name: 'Episode 17',
+			      	sources: [{ src: 'http://81.111.188.191/media/new_tv_shows/TheFlash/Season01/S01E17.mp4', type: 'video/mp4' }]
+			    	},
+			    	{
+			      	name: 'Episode 18',
+			      	sources: [{ src: 'http://81.111.188.191/media/new_tv_shows/TheFlash/Season01/S01E18.mp4', type: 'video/mp4' }]
+			    	},
+			    	{
+			      	name: 'Episode 19',
+			      	sources: [{ src: 'http://81.111.188.191/media/new_tv_shows/TheFlash/Season01/S01E19.mp4', type: 'video/mp4' }]
+			    	},
+			    	{
+			      	name: 'Episode 20',
+			      	sources: [{ src: 'http://81.111.188.191/media/new_tv_shows/TheFlash/Season01/S01E20.mp4', type: 'video/mp4' }]
+			    	},
+			    	{
+			      	name: 'Episode 21',
+			      	sources: [{ src: 'http://81.111.188.191/media/new_tv_shows/TheFlash/Season01/S01E21.mp4', type: 'video/mp4' }]
+			    	},
+			    	{
+			      	name: 'Episode 22',
+			      	sources: [{ src: 'http://81.111.188.191/media/new_tv_shows/TheFlash/Season01/S01E22.mp4', type: 'video/mp4' }]
+			    	},
+			    	{
+			      	name: 'Episode 23',
+			      	sources: [{ src: 'http://81.111.188.191/media/new_tv_shows/TheFlash/Season01/S01E23.mp4', type: 'video/mp4' }]
+			    	}
+			    ]);
+
+				//Loads the video into the player ready to play
+				player.playlist.first();
+				player.playlist.autoadvance(0);
+				Materialize.toast('Playlist Loaded, Please press play at the top of the page!', 3000)
+				return;
+	}
 
