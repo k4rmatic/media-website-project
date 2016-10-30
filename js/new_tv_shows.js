0b05267546b354fb56cdd6eb8331487b138002e1
k4rmatic <aqiba123@gmail.com>
2016-10-30 17:11:45 +0000

    added the big bang

--- js/new_tv_shows.js
+++ js/new_tv_shows.js
@@ -1110,3 +1110,189 @@
 				return;
 	}
 
+//The bigbang
+	//the bigbang Season 1
+	function TheBigBang_s1() {
+				var player = videojs('video');
+			    player.playlist([
+			   		{
+			      	name: 'Season 1 - Episode 1',
+			      	sources: [{ src: 'http://81.111.188.191/media/new_tv_shows/TheBigBangTheory/Season01/S01E01.mp4', type: 'video/mp4' }]
+					},
+			    	{
+			      	name: 'Episode 2',
+			      	sources: [{ src: 'http://81.111.188.191/media/new_tv_shows/TheBigBangTheory/Season01/S01E02.mp4', type: 'video/mp4' }]
+			    	},
+			    	{
+			      	name: 'Episode 3',
+			      	sources: [{ src: 'http://81.111.188.191/media/new_tv_shows/TheBigBangTheory/Season01/S01E03.mp4', type: 'video/mp4' }]
+			    	},
+			    	{
+			      	name: 'Episode 4',
+			      	sources: [{ src: 'http://81.111.188.191/media/new_tv_shows/TheBigBangTheory/Season01/S01E04.mp4', type: 'video/mp4' }]
+			    	},
+			    	{
+			      	name: 'Episode 5',
+			      	sources: [{ src: 'http://81.111.188.191/media/new_tv_shows/TheBigBangTheory/Season01/S01E05.mp4', type: 'video/mp4' }]
+			    	},
+			    	{
+			      	name: 'Episode 6',
+			      	sources: [{ src: 'http://81.111.188.191/media/new_tv_shows/TheBigBangTheory/Season01/S01E06.mp4', type: 'video/mp4' }]
+			    	},
+			    	{
+			      	name: 'Episode 7',
+			      	sources: [{ src: 'http://81.111.188.191/media/new_tv_shows/TheBigBangTheory/Season01/S01E07.mp4', type: 'video/mp4' }]
+			    	},
+			    	{
+			      	name: 'Episode 8',
+			      	sources: [{ src: 'http://81.111.188.191/media/new_tv_shows/TheBigBangTheory/Season01/S01E08.mp4', type: 'video/mp4' }]
+			    	},
+			    	{
+			      	name: 'Episode 9',
+			      	sources: [{ src: 'http://81.111.188.191/media/new_tv_shows/TheBigBangTheory/Season01/S01E09.mp4', type: 'video/mp4' }]
+			    	},
+			    	{
+			      	name: 'Episode 10',
+			      	sources: [{ src: 'http://81.111.188.191/media/new_tv_shows/TheBigBangTheory/Season01/S01E10.mp4', type: 'video/mp4' }]
+			    	},
+			    	{
+			      	name: 'Episode 11',
+			      	sources: [{ src: 'http://81.111.188.191/media/new_tv_shows/TheBigBangTheory/Season01/S01E11.mp4', type: 'video/mp4' }]
+			    	},
+			    	{
+			      	name: 'Episode 12',
+			      	sources: [{ src: 'http://81.111.188.191/media/new_tv_shows/TheBigBangTheory/Season01/S01E12.mp4', type: 'video/mp4' }]
+			    	},
+			    	{
+			      	name: 'Episode 13',
+			      	sources: [{ src: 'http://81.111.188.191/media/new_tv_shows/TheBigBangTheory/Season01/S01E13.mp4', type: 'video/mp4' }]
+			    	},
+			    	{
+			      	name: 'Episode 14',
+			      	sources: [{ src: 'http://81.111.188.191/media/new_tv_shows/TheBigBangTheory/Season01/S01E14.mp4', type: 'video/mp4' }]
+			    	},
+			    	{
+			      	name: 'Episode 15',
+			      	sources: [{ src: 'http://81.111.188.191/media/new_tv_shows/TheBigBangTheory/Season01/S01E15.mp4', type: 'video/mp4' }]
+			    	},
+			    	{
+			      	name: 'Episode 16',
+			      	sources: [{ src: 'http://81.111.188.191/media/new_tv_shows/TheBigBangTheory/Season01/S01E16.mp4', type: 'video/mp4' }]
+			    	},
+			    	{
+			      	name: 'Episode 17',
+			      	sources: [{ src: 'http://81.111.188.191/media/new_tv_shows/TheBigBangTheory/Season01/S01E17.mp4', type: 'video/mp4' }]
+			    	}
+			    ]);
+
+				//Loads the video into the player ready to play
+				player.playlist.first();
+				player.playlist.autoadvance(0);
+				Materialize.toast('Playlist Loaded, Please press play at the top of the page!', 3000)
+				return;
+	}
+	//the bigbang Season 2
+	function TheBigBang_s2() {
+				var player = videojs('video');
+			    player.playlist([
+			   		{
+			      	name: 'Season 2 - Episode 1',
+			      	sources: [{ src: 'http://81.111.188.191/media/new_tv_shows/TheBigBangTheory/Season02/S02E01.mp4', type: 'video/mp4' }]
+					},
+			    	{
+			      	name: 'Episode 2',
+			      	sources: [{ src: 'http://81.111.188.191/media/new_tv_shows/TheBigBangTheory/Season02/S02E02.mp4', type: 'video/mp4' }]
+			    	},
+			    	{
+			      	name: 'Episode 3',
+			      	sources: [{ src: 'http://81.111.188.191/media/new_tv_shows/TheBigBangTheory/Season02/S02E03.mp4', type: 'video/mp4' }]
+			    	},
+			    	{
+			      	name: 'Episode 4',
+			      	sources: [{ src: 'http://81.111.188.191/media/new_tv_shows/TheBigBangTheory/Season02/S02E04.mp4', type: 'video/mp4' }]
+			    	},
+			    	{
+			      	name: 'Episode 5',
+			      	sources: [{ src: 'http://81.111.188.191/media/new_tv_shows/TheBigBangTheory/Season02/S02E05.mp4', type: 'video/mp4' }]
+			    	},
+			    	{
+			      	name: 'Episode 6',
+			      	sources: [{ src: 'http://81.111.188.191/media/new_tv_shows/TheBigBangTheory/Season02/S02E06.mp4', type: 'video/mp4' }]
+			    	},
+			    	{
+			      	name: 'Episode 7',
+			      	sources: [{ src: 'http://81.111.188.191/media/new_tv_shows/TheBigBangTheory/Season02/S02E07.mp4', type: 'video/mp4' }]
+			    	},
+			    	{
+			      	name: 'Episode 8',
+			      	sources: [{ src: 'http://81.111.188.191/media/new_tv_shows/TheBigBangTheory/Season02/S02E08.mp4', type: 'video/mp4' }]
+			    	},
+			    	{
+			      	name: 'Episode 9',
+			      	sources: [{ src: 'http://81.111.188.191/media/new_tv_shows/TheBigBangTheory/Season02/S02E09.mp4', type: 'video/mp4' }]
+			    	},
+			    	{
+			      	name: 'Episode 10',
+			      	sources: [{ src: 'http://81.111.188.191/media/new_tv_shows/TheBigBangTheory/Season02/S02E10.mp4', type: 'video/mp4' }]
+			    	},
+			    	{
+			      	name: 'Episode 11',
+			      	sources: [{ src: 'http://81.111.188.191/media/new_tv_shows/TheBigBangTheory/Season02/S02E11.mp4', type: 'video/mp4' }]
+			    	},
+			    	{
+			      	name: 'Episode 12',
+			      	sources: [{ src: 'http://81.111.188.191/media/new_tv_shows/TheBigBangTheory/Season02/S02E12.mp4', type: 'video/mp4' }]
+			    	},
+			    	{
+			      	name: 'Episode 13',
+			      	sources: [{ src: 'http://81.111.188.191/media/new_tv_shows/TheBigBangTheory/Season02/S02E13.mp4', type: 'video/mp4' }]
+			    	},
+			    	{
+			      	name: 'Episode 14',
+			      	sources: [{ src: 'http://81.111.188.191/media/new_tv_shows/TheBigBangTheory/Season02/S02E14.mp4', type: 'video/mp4' }]
+			    	},
+			    	{
+			      	name: 'Episode 15',
+			      	sources: [{ src: 'http://81.111.188.191/media/new_tv_shows/TheBigBangTheory/Season02/S02E15.mp4', type: 'video/mp4' }]
+			    	},
+			    	{
+			      	name: 'Episode 16',
+			      	sources: [{ src: 'http://81.111.188.191/media/new_tv_shows/TheBigBangTheory/Season02/S02E16.mp4', type: 'video/mp4' }]
+			    	},
+			    	{
+			      	name: 'Episode 17',
+			      	sources: [{ src: 'http://81.111.188.191/media/new_tv_shows/TheBigBangTheory/Season02/S02E17.mp4', type: 'video/mp4' }]
+			    	},
+			    	{
+			      	name: 'Episode 18',
+			      	sources: [{ src: 'http://81.111.188.191/media/new_tv_shows/TheBigBangTheory/Season02/S02E18.mp4', type: 'video/mp4' }]
+			    	},
+			    	{
+			      	name: 'Episode 19',
+			      	sources: [{ src: 'http://81.111.188.191/media/new_tv_shows/TheBigBangTheory/Season02/S02E19.mp4', type: 'video/mp4' }]
+			    	},
+			    	{
+			      	name: 'Episode 20',
+			      	sources: [{ src: 'http://81.111.188.191/media/new_tv_shows/TheBigBangTheory/Season02/S02E20.mp4', type: 'video/mp4' }]
+			    	},
+			    	{
+			      	name: 'Episode 21',
+			      	sources: [{ src: 'http://81.111.188.191/media/new_tv_shows/TheBigBangTheory/Season02/S02E21.mp4', type: 'video/mp4' }]
+			    	},
+			    	{
+			      	name: 'Episode 22',
+			      	sources: [{ src: 'http://81.111.188.191/media/new_tv_shows/TheBigBangTheory/Season02/S02E22.mp4', type: 'video/mp4' }]
+			    	},
+			    	{
+			      	name: 'Episode 23',
+			      	sources: [{ src: 'http://81.111.188.191/media/new_tv_shows/TheBigBangTheory/Season02/S02E23.mp4', type: 'video/mp4' }]
+			    	}
+			    ]);
+
+				//Loads the video into the player ready to play
+				player.playlist.first();
+				player.playlist.autoadvance(0);
+				Materialize.toast('Playlist Loaded, Please press play at the top of the page!', 3000)
+				return;
+	}
+
